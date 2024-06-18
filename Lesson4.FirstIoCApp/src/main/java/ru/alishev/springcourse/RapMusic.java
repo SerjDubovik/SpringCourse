025694a913aa9471a6d256965635bd87865c00c5
Serj <mail@pufr.ru>
2024-06-18 23:40:14 +0300

    задание из 4-го урока

--- Lesson4.FirstIoCApp/src/main/java/ru/alishev/springcourse/RapMusic.java
+++ Lesson4.FirstIoCApp/src/main/java/ru/alishev/springcourse/RapMusic.java
@@ -1,9 +1,9 @@
 package ru.alishev.springcourse;
 
+public class RapMusic implements Music{
 
-public class RockMusic implements Music {
     @Override
     public String getSong() {
-        return "Wind cries Mary";
+        return "Eminem Shou";
     }
 }
